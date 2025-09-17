72087625518794f1f9d06b3e7b93c44a523e1b99
zagir_lek <gadzimirzoevz05@gmail.com>
2025-09-17 16:14:36 +0300

    feature: added mock Auth repository

---
+++ app/src/main/java/com/zagirlek/nytimes/data/repository/MockAuthRepository.kt
@@ -0,0 +1,27 @@
+package com.zagirlek.nytimes.data.repository
+
+import android.content.Context
+import com.zagirlek.nytimes.domain.model.AuthToken
+import com.zagirlek.nytimes.domain.repository.AuthRepository
+import kotlinx.coroutines.delay
+
+class MockAuthRepository(): AuthRepository {
+
+    override suspend fun getCurrToken(): AuthToken? {
+        delay(2000)
+        return null
+    }
+
+    override suspend fun login(
+        login: String,
+        password: String
+    ): AuthToken {
+        delay(2000)
+        return AuthToken.DEFAULT_TOKEN
+    }
+
+    override suspend fun getTokenWithoutLogin(): AuthToken {
+        return AuthToken.DEFAULT_TOKEN
+    }
+
+}
---
+++ app/src/main/java/com/zagirlek/nytimes/domain/repository/AuthRepository.kt
@@ -0,0 +1,11 @@
+package com.zagirlek.nytimes.domain.repository
+
+import com.zagirlek.nytimes.domain.model.AuthToken
+
+interface AuthRepository {
+    suspend fun getCurrToken(): AuthToken?
+
+    suspend fun login(login: String, password: String): AuthToken
+
+    suspend fun getTokenWithoutLogin(): AuthToken
+}
